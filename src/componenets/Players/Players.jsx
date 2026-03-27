4b14692908e6a7f2b1a383d31bcbd976f97be066
ashiqurrhmn <arakash115@gmail.com>
2026-03-28 02:06:58 +0600

    fixed

--- src/componenets/Players/Players.jsx
+++ src/componenets/Players/Players.jsx
@@ -9,26 +9,28 @@ const Players = ({ promise, setBalance, balance }) => {
 
   return (
     <>
-      <div className="w-10/12 mx-auto flex justify-between items-center">
+    <div className="">
+      <div className="w-10/12 mx-auto flex flex-col md:flex-row items-center justify-between">
         {btnType === "available" ? (
-          <div className="my-10 text-3xl font-bold">Available Players</div>
+          <div className="my-10 text-[20px] md:text-3xl font-bold">Available Players</div>
         ) : (
-          <div className="my-10 text-3xl font-bold">Selected Players ({selectedPlayers.length}/{data.length})</div>
+          <div className="my-10 text-[20px] md:text-3xl font-bold">Selected Players ({selectedPlayers.length}/{data.length})</div>
         )}
-        <div>
+        <div className="mb-2">
           <button
-            className={`${btnType === "available" ? "bg-[#E7FE29]" : "bg-gray-100"} w-40 px-4 py-2 md:px-6 md:py-3 md:rounded-l-lg font-semibold cursor-pointer border-gray-200 border md:border-r-0`}
+            className={`${btnType === "available" ? "bg-[#E7FE29]" : "bg-gray-100"} rounded-full md:w-40 px-4 py-2 md:px-6 md:py-3  font-semibold cursor-pointer border-gray-200 border `}
             onClick={() => setBtnType("available")}
           >
             Available
           </button>
           <button
-            className={`${btnType === "selected" ? "bg-[#E7FE29]" : "bg-gray-100"} w-40 px-4 py-2 md:px-6 md:py-3 md:rounded-r-lg font-semibold cursor-pointer border-gray-200 border md:border-l-0`}
+            className={`${btnType === "selected" ? "bg-[#E7FE29]" : "bg-gray-100"} rounded-full md:w-40 px-4 py-2 md:px-6 md:py-3  font-semibold cursor-pointer border-gray-200 border `}
             onClick={() => setBtnType("selected")}
           >
             Selected ({selectedPlayers.length})
           </button>
         </div>
+      </div>
       </div>
       {btnType === "available" ? (
         <AvailablePlayers data={data} setBalance={setBalance} balance={balance} setSelectedPlayers={setSelectedPlayers} selectedPlayers={selectedPlayers}></AvailablePlayers>
